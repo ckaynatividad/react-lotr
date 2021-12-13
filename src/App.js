24678a094656b4351b1896473259a15b5273e81f
ckaynatividad <natividad.ckay@gmail.com>
2021-12-13 15:45:45 -0800

    fixed nonrendering, changed item names to new supabase names

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   }, []);
 
   const getFilms = async () => {
+    
     // Add your code here!
     // 1. Get data using fetch from https://the-one-api.dev/v2/movie/ (don't forget to set your header!)
     // 2. Transform the response so that films contains nested arrays of:
@@ -30,17 +31,17 @@ function App() {
     const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/films`, {
       headers: {
         apikey: process.env.REACT_APP_SUPABASE_KEY,
-        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
       },
     });
     // console.log(resp);
     const data = await resp.json();
     // console.log(data);
-    const filmsMunged = data.docs.map((item) => [
-      item.name,
-      item.name.trim().toLowerCase().split(' ').join('-'),
-      item.boxOfficeRevenueInMillions,
-      item.academyAwardNominations,
+    const filmsMunged = data.map((item) => [
+      item.title,
+      item.title.trim().toLowerCase().split(' ').join('-'),
+      item.box_office_total,
+      item.academy_award_nominations,
     ]);
     // setFilms(filmsMunged);
     // console.log(filmsMunged);
@@ -64,11 +65,13 @@ function App() {
     const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/characters`, {
       headers: {
         apikey: process.env.REACT_APP_SUPABASE_KEY,
-        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
       },
     });
+    
     const data = await resp.json();
-    const charMunged = data.docs.map((item) => ({
+    console.log(data);
+    const charMunged = data.map((item) => ({
       ...item,
       dates: item.birth && item.death ? `${item.birth} - ${item.death}` : 'Unknown',
     }));
